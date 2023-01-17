7a478aaaf8414869d41563e3a40dc45392782da7
Andrew Shalaenko <ashalaenko@gmail.com>
2023-01-17 16:54:12 +0000

    commit generated fileds

--- asana/resources/gen/__init__.py
+++ asana/resources/gen/__init__.py
@@ -1,1 +1,9 @@
-# init file
+# do not import all endpoints into this module because that uses a lot of memory and stack frames
+# if you need the ability to import all endpoints from this module, import them with
+# from asana.apis.tag_to_api import tag_to_api
+
+import enum
+
+
+class TagValues(str, enum.Enum):
+    ATTACHMENTS = "Attachments"
